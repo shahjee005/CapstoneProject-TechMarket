305c7ec0d58a9ed5e32a6cb4ff448e4fb154b9eb
Mohammad <adawi83@hotmail.com>
2020-06-04 12:31:44 -0600

    logo update

--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,52 +1,60 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import React, { Component} from 'react';
+import { Collapse, Navbar, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+    ////////////////////////////////////////////////
+ static displayName = NavMenu.name;
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+ constructor() {
+     super();
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+     this.toggleNavbar = this.toggleNavbar.bind(this);
+     this.state = { collapsed: true };
+ }
 
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-         <img id="logo-nav" className="img-fluid img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/5/52/MYLAST_LOGO.png" alt="logo" />
-         
-       <NavbarToggler onClick={this.toggleNavbar} className="mr-2" id="toggler" />
-            
-              <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/validate">validate</NavLink>
-                </NavItem>
-                <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/SignIn">SignIn/SignUp</NavLink>
-               </NavItem>
-              </ul>
-              </Collapse>
-              <button id="cart" type="submit"><i className="glyphicon glyphicon-shopping-cart" /> My Cart</button>
-          </Container>
-        </Navbar>
-        </header>
-    );
-  }
+ toggleNavbar() {
+     this.setState({
+         collapsed: !this.state.collapsed
+     });
+ }
+
+ render() {
+     return (
+         <Navbar className="navbar-expand-sm navbar-toggleable-sm navbar-inverse border-bottom box-shadow mb-1" >
+             <Link to={"/"}>
+                 <img id="logo-nav" className="img-fluid img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/e/e5/FINALLOGO.png" alt="logo" />
+             </Link>
+                 <NavbarToggler onClick={this.toggleNavbar} className="ml-auto" id="toggler" />
+
+                     
+                     <Collapse className="d-sm-inline-flex flex-sm-row-reverse text-light" isOpen={!this.state.collapsed} navbar>
+                         <ul className="navbar-nav flex-grow">
+                             <NavItem>
+                                 <NavLink tag={Link} className="text-light" to="/">Home</NavLink>
+                             </NavItem>
+                            {/*<NavItem>
+                                 <NavLink tag={Link} className="text-dark" to="/validate">validate</NavLink>
+                             </NavItem>*/}
+                             {this.props.isLogin === false && // if not log in yet
+                                 <NavItem>
+                             <NavLink tag={Link} className="text-light" to="/login">Login/Register</NavLink>
+                                 </NavItem>
+                             }
+                             {this.props.isLogin === true && //if status is logged in
+                                 <NavItem onClick={this.props.handleLogout}>
+                             <NavLink tag={Link} className="text-light" to="/">Logout</NavLink>
+                                 </NavItem>
+                             }
+                           
+                         </ul>
+                     </Collapse>
+                     <Link to="/cart" className="cccard">
+                         <button id="cart" type="submit"><i className="glyphicon ml-auto glyphicon-shopping-cart" /> Cart</button>
+                     </Link>
+             </Navbar>
+     );
+ }
 }
