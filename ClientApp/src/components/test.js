f178dceb8ec2dacb65754cfa83b303982f4fb1af
Mohammad <adawi83@hotmail.com>
2020-06-04 12:28:33 -0600

    delete weatherforcast.cs

---
+++ ClientApp/src/components/test.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+
+export class test extends Component {
+    static displayName = test.name;
+
+    constructor() {
+        constructor(props) {
+            super(props);
+        this.state = { test: [], done: false };
+    }
+
+    componentDidMount() {
+        fetch('/api/validate')
+            .then(res => res.json())
+            .then(result => this.setState({ test: result.test }))
+    }
+
+    render() {
+        if (!this.state.done) {
+            return (
+                <div>
+                    
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                    {this.state.test}
+                </div>
+            );
+        }
+    }
+}
