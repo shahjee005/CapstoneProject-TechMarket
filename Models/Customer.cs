69f3aa5787ae62876f494f18b3ccb5327a399c21
Mahmoud Maguid <mahmoud.maguid@yahoo.ca>
2020-06-04 13:05:39 -0600

    add Customer2

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,11 +1,27 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TECHMarket.Models
+namespace TechMarket.Model
 {
     public class Customer
     {
+        [Key]
+        public int CustomerId { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string PasswordHashed { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime LastLogin { get; set; }
+        public ICollection<Cart> Cart { get; set; }
+        // public ICollection<CustomerShippingAddresses> CustomerShippingAddresses { get; set; }
+        public ICollection<Orders> Orders { get; set; }
     }
 }
