eb33e6ce5f03d539169e4cfbed3ca11f6d0408ac
Mohammad <adawi83@hotmail.com>
2020-06-08 10:49:20 -0600

    version v1

---
+++ ClientApp/src/components/Contact.js
@@ -0,0 +1,21 @@
+﻿import React, { Component } from 'react';
+
+export class Contact extends Component {
+
+    
+
+    render() {
+        return (
+            <div class="wrapper fadeInDown">
+                <div id="formContent">
+                    <h1>CounterTest</h1>
+
+                <p>This is a simple example of a React component.</p>
+
+              
+                </div>
+            </div>
+
+        );
+    }
+}
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -23,37 +23,42 @@ export class NavMenu extends Component {
 
  render() {
      return (
-         <Navbar className="navbar-expand-sm navbar-toggleable-sm navbar-inverse border-bottom box-shadow mb-1" >
+         <Navbar className="navbar navbar-expand-lg   navbar-light shadow " bg="light" expand="lg" variant="light">
              <Link to={"/"}>
                  <img id="logo-nav" className="img-fluid img-responsive" src="https://upload.wikimedia.org/wikipedia/commons/e/e5/FINALLOGO.png" alt="logo" />
              </Link>
+             
                  <NavbarToggler onClick={this.toggleNavbar} className="ml-auto" id="toggler" />
-
-                     
-                     <Collapse className="d-sm-inline-flex flex-sm-row-reverse text-light" isOpen={!this.state.collapsed} navbar>
-                         <ul className="navbar-nav flex-grow">
-                             <NavItem>
-                                 <NavLink tag={Link} className="text-light" to="/">Home</NavLink>
-                             </NavItem>
-                            {/*<NavItem>
-                                 <NavLink tag={Link} className="text-dark" to="/validate">validate</NavLink>
-                             </NavItem>*/}
-                             {this.props.isLogin === false && // if not log in yet
-                                 <NavItem>
-                             <NavLink tag={Link} className="text-light" to="/login">Login/Register</NavLink>
-                                 </NavItem>
-                             }
-                             {this.props.isLogin === true && //if status is logged in
-                                 <NavItem onClick={this.props.handleLogout}>
-                             <NavLink tag={Link} className="text-light" to="/">Logout</NavLink>
-                                 </NavItem>
-                             }
+             <Collapse className="d-sm-inline-flex " isOpen={!this.state.collapsed} navbar>
+                 <NavLink tag={Link} className="dark" to="/">Home</NavLink>
+                 <NavLink tag={Link} className="dark" to="/About">About-us</NavLink>
+                 <NavLink tag={Link} className="dark" to="/Contact">Contact-Us</NavLink>
+                 <NavLink tag={Link} className="dark" to="/Findus">Find-us</NavLink>
+               
+                
                            
-                         </ul>
-                     </Collapse>
-                     <Link to="/cart" className="cccard">
+             </Collapse>
+
+                              <NavLink >
+
+                     <Link to="/cart" className="cccard ">
                          <button id="cart" type="submit"><i className="glyphicon ml-auto glyphicon-shopping-cart" /> Cart</button>
-                     </Link>
+                 </Link>
+             </NavLink>
+
+             <span>{this.props.isLogin === false && // if not log in yet
+                 <NavLink tag={Link} className="dark" to="/login"> <span title="Click to sigin in "></span>
+ <i class="fa fa-user" aria-hidden="true"></i>/
+                     <i class="fas fa-user-plus"></i>
+                 </NavLink>
+
+             }
+             </span>
+             <span>
+                 {this.props.isLogin === true && //if status is logged in
+                     <NavLink tag={Link} className="dark" to="/" onClick={this.props.handleLogout}><i class="fas fa-sign-out-alt"></i></NavLink>
+                 }
+             </span>
              </Navbar>
      );
  }
