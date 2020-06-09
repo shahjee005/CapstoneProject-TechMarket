c0b3eaf39dfa4637fc6efab01baca0d4aca6b66b
Mahmoud Maguid <mahmoud.maguid@yahoo.ca>
2020-06-08 19:27:46 -0600

    add functions to Data folder

---
+++ Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechMarket.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechMarket.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        
+        }
+        public DbSet<Customer> Customer { get; set; }       
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<ProductDetails> ProductDetails { get; set; }
+        public DbSet<Cart> Cart { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.SeedCustomers();        
+            modelBuilder.SeedProducts();
+            modelBuilder.SeedProductDetails();
+        }
+    }
+}
---
+++ Data/Functions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.EntityFrameworkCore;
+
+namespace TechMarket.Data
+{
+    public class Functions
+    {
+        //Function Hash password give hashed password and salted password
+        public void HashPassword(string pass, out byte[] salted, out string hashed)
+        {
+            salted = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salted);
+            }
+            hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: pass,
+                salt: salted,
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+        }
+    }
+
+}
